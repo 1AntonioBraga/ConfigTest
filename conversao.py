9c91301a9c20e8ff1c054fd5060743da9cfe9147
1AntonioBraga <antoniopbragajr@gmail.com>
2026-06-24 22:24:36 -0400

    Second For test

---
+++ conversao.py
@@ -0,0 +1,23 @@
+preco = 10 
+idade = 33
+print(preco)
+
+print(preco/2)
+
+print(preco//2)
+
+
+
+print(str(preco))
+
+print(str(idade))
+
+
+print(float(preco))
+
+print(int(idade))
+
+
+idade = idade // 5 # Note que após esse ponto a idade irá exibir em formato inteiro
+
+print(idade)
---
+++ variaveis.py
@@ -0,0 +1,20 @@
+BRAZILIAN_STATES = ["SP","AM","RO"]
+age = 25
+name = "Giovani"
+
+print(f'Meu nome é {name} e tenho {age} anos')
+
+
+age = 32
+name = "Samanta"
+
+print(f'Meu nome é {name} e tenho {age} anos e moro no estado {BRAZILIAN_STATES[1]}')
+
+
+
+print({BRAZILIAN_STATES[2]})
+
+# Boas Práticas:
+# O padrão de nomes deve ser snake case
+# Escolher nomes sugestivos
+# Nome de constantes todo em maiúsculo
