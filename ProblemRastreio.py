369c179dae01723904c2f797d82eed73b4ea4b1a
1AntonioBraga <antoniopbragajr@gmail.com>
2026-06-28 14:15:52 -0400

    Comparador Rastreio added

---
+++ ProblemRastreio.py
@@ -0,0 +1,12 @@
+#Faça um programa que receba dois códigos de rastreio e informe se eles são iguais ou diferentes.
+
+#Variáveis
+R1 = input("Digite o primeiro código de rastreio: ")
+R2 = input("Digite o segundo código de rastreio: ")
+
+if R1 == R2:
+    print(f"{R1} e {R2} são iguais !!!")
+else:
+    print(f"{R1} e {R2} são diferentes !!!")
+
+    
